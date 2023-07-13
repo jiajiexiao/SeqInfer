1bf83db68ad3edf5a966eab7de7bd5507a5e4841
jjxiao <{ID}+{username}@users.noreply.github.com>
2023-07-12 21:18:26 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# SeqLearn Package
+
+A package for learning from sequences. 
+
+## Seq Generator
+- Sampling with uniform prior or PWM 
+- VAE
+- Diffusion 
+- GAN
+- Seq2seq
+
+## Seq learner
+
+- Encoder
+
+CNN/LSTM/Transformer/Kmer/One-hot 
+-- Auto-encoder
+-- Supervised auto-encoder 
+--
+
+
+
+## Example 
+- Classification 
+- Regression 
+- multiple instance learning 
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="seqlearn",
+    version="0.0.1",
+    author="Jiajie Xiao",
+    author_email="jiajiexiao@gmail.com",
+    description="A package to learn from sequence",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jiajiexiao/seqlearn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
