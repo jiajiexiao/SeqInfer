b0216b44d53160d7555efef37938fb2cc2b55fc1
jjxiao <{ID}+{username}@users.noreply.github.com>
2023-08-30 14:31:14 -0700

    mean reduction rather than sum

--- src/seqinfer/infer/components/losses.py
+++ src/seqinfer/infer/components/losses.py
@@ -18,7 +18,9 @@ class L1RegularizationLoss(nn.Module):
     def forward(self, model: nn.Module) -> torch.Tensor:
         """Computes the L1 loss on the weights of the model."""
         l1_loss = 0.0
+        num_parameters = 0
         for name, param in model.named_parameters():
             if "bias" not in name and param.requires_grad:
-                l1_loss += torch.mean(torch.abs(param))
-        return self.weight_decay * l1_loss
+                l1_loss += torch.sum(torch.abs(param))
+                num_parameters += param.numel()
+        return self.weight_decay * l1_loss / num_parameters
